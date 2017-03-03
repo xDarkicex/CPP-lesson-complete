44a7d97bb452a0c4b313c2426a85d3f4d267658f
Gentry Rolofson <xdarkicex@gmail.com>
2017-03-02 22:14:13 -0800

    first commit

---
+++ lession-1/sources/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main () {
+  std::cout << 2017 << " is today's years." << std::endl;
+  std::cout << 3 << " is today's month." << std::endl;
+  std::cout << 1 << " is today's day." << std::endl;
+  return 0;
+}
